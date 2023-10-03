2231245c9e915ab2c7630e4ecd61fe54e21d5e13
stefan0704 <stefan07041970@gmail.com>
2023-10-04 00:36:04 +0200

    ft_memmove

---
+++ ft_memmove.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smoraru <smoraru@student.42madrid.>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/26 22:05:53 by smoraru           #+#    #+#             */
+/*   Updated: 2023/09/27 21:17:50 by smoraru          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void    *ft_memmove(void *dest, const void *src, size_t n)
+{
+    unsigned char       *dp;
+    const unsigned char *sp;
+    size_t              i;
+
+    if(!dest && !src)
+    {
+        return (0);
+    }
+}
