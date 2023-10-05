ea3e72a8f199681e6d8861b58124df9a6b129b87
stefan0704 <stefan07041970@gmail.com>
2023-10-05 14:17:48 +0200

    patata

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -23,11 +23,11 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 		return (size + ft_strlen(src));
 	i = ft_strlen(dst);
 	j = 0;
-	while (src[j] && j < size)
+	while (src[j] && j < size - 1)
 	{
 		dst[i + j] = src[j];
 		j++;
 	}
 	dst[i + j] = '\0';
-	return (i + (j - 1));
+	return (i + j);
 }
