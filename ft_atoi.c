cc54f20616069d5ac738f00325e50189e8a15db8
Stefan Alexandru Moraru <smoraru@c2r10s6.42madrid.com>
2023-10-06 22:03:30 +0200

    cositas

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,34 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: smoraru <smoraru@student.42madrid.>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/26 22:05:53 by smoraru           #+#    #+#             */
-/*   Updated: 2023/10/06 21:19:35 by smoraru          ###   ########.fr       */
+/*   Created: 2023/10/06 21:20:02 by smoraru           #+#    #+#             */
+/*   Updated: 2023/10/06 21:20:04 by smoraru          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-char	*ft_strnstr(const char *a, const char *b, size_t n)
+int	ft_atoi(char *str)
 {
-	size_t	i;
-	size_t	j;
+	int	result;
+	int	sign;
+	int	i;
 
+	result = 0;
+	sign = 1;
 	i = 0;
-	if (!a || !b)
-		return (NULL);
-	if (!b || !b[0])
-		return ((char *)a);
-	while (a[i] && i < n)
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
+		i++;
+	if (str[i] == '+' || str[i] == '-')
 	{
-		j = 0;
-		while (a[i + j] && b[j] && i + j < n && a[i + j] == b[j])
-			j++;
-		if (!b[j])
-			return ((char *)&a[i]);
+		if (str[i] == '-')
+			sign *= -1;
 		i++;
 	}
-	return (NULL);
+	while (ft_isdigit(str[i]))
+	{
+		result *= 10;
+		result += str[i] - '0';
+		i++;
+	}
+	return (result * sign);
 }
